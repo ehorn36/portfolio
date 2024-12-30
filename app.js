7a4170d4ab7c6be777884e862b060961d447e138
ehorn36 <62220173+ehorn36@users.noreply.github.com>
2024-12-30 08:31:20 -0500

    updating folders for NameCheck hosting requirements

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 
 // Include the express module and assign it to a constant named 'express'.
 const express = require('express');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
 // The express() function is a top-level function exported by the express module. 
 const app = express();
 const port = 3000;
 const path = require('path'); 
 
-// Instantiate the 'ejs' templating engine.
-app.set('view engine', 'ejs');
-
 // Define the location of views.
-app.set('views', path.join(__dirname, './views'));
+app.use(express.static(path.join(__dirname, 'views')));
 
 // Define the location of the public directory for css, js, and imgs.
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-
+// Instantiate the 'ejs' templating engine.
+app.set('views', path.join(__dirname, 'views'));
+app.set('public', path.join(__dirname, 'public'));
+app.set('view engine', 'ejs');
 
 // *** GET routes ***
 app.get('/', (req, res) => {
@@ -32,8 +32,7 @@ app.get('/contact', (req, res) => {
   res.render('contact.ejs')
 })
 
-
 // Specify which port we want the server to listen for requests.
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}: http://localhost:${port}/`)
+  console.log('main app started on http://localhost:' + port + '; press Ctrl-C to terminate.')
 })
